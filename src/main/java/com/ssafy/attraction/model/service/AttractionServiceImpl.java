ddad3f36ffb45037ba1be9f83a244fbe2164f3ab
JeongHun <wjdgns7040@gmail.com>
2023-05-02 11:40:02 +0900

    Update Member
    Update Member

--- src/main/java/com/ssafy/attraction/model/service/AttractionServiceImpl.java
+++ src/main/java/com/ssafy/attraction/model/service/AttractionServiceImpl.java
@@ -1,31 +1,31 @@
-package com.ssafy.attraction.model.service;
-
-import java.util.List;
-
-import com.ssafy.attraction.model.AttractionDto;
-import com.ssafy.attraction.model.dao.AttractionDao;
-import com.ssafy.attraction.model.dao.AttractionDaoImpl;
-
-public class AttractionServiceImpl implements AttractionService 
-{
-	private static AttractionService attractionService = new  AttractionServiceImpl();
-	private AttractionDao attractionDao;
-
-	private AttractionServiceImpl() {
-		attractionDao = AttractionDaoImpl.getAttractionDao();
-	}
-
-	public static AttractionService getAttractionService() {
-		return attractionService;
-	}
-
-	@Override
-	public void insertAttraction(AttractionDto attractionDto) throws Exception {
-		attractionDao.insertAttraction(attractionDto);
-	}
-
-	@Override
-	public List<AttractionDto> searchAttractionByLatLon(float latitude, float longitude, float meter) throws Exception {
-		return attractionDao.searchAttractionByLatLon(latitude, longitude, meter);
-	}
-}
+//package com.ssafy.attraction.model.service;
+//
+//import java.util.List;
+//
+//import com.ssafy.attraction.model.AttractionDto;
+//import com.ssafy.attraction.model.dao.AttractionDao;
+//import com.ssafy.attraction.model.dao.AttractionDaoImpl;
+//
+//public class AttractionServiceImpl implements AttractionService 
+//{
+//	private static AttractionService attractionService = new  AttractionServiceImpl();
+//	private AttractionDao attractionDao;
+//
+//	private AttractionServiceImpl() {
+//		attractionDao = AttractionDaoImpl.getAttractionDao();
+//	}
+//
+//	public static AttractionService getAttractionService() {
+//		return attractionService;
+//	}
+//
+//	@Override
+//	public void insertAttraction(AttractionDto attractionDto) throws Exception {
+//		attractionDao.insertAttraction(attractionDto);
+//	}
+//
+//	@Override
+//	public List<AttractionDto> searchAttractionByLatLon(float latitude, float longitude, float meter) throws Exception {
+//		return attractionDao.searchAttractionByLatLon(latitude, longitude, meter);
+//	}
+//}
