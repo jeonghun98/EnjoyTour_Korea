74e395d85f568afe5541bda4ec5d84a4ced8a613
hmson18 <sondo100@hanmail.net>
2023-05-22 11:11:27 +0900

    Update FE - hotplace

--- FE/enjoytour_korea/src/components/hotplace/HotplaceView.vue
+++ FE/enjoytour_korea/src/components/hotplace/HotplaceView.vue
@@ -1,18 +1,61 @@
 <template>
   <div>
     {{hotplace.hotplaceNo}}번 핫플레이스 상세보기 화면입니다
+    <!-- 핫플레이스 이미지 -->
+    <span>
+      <div>
+        <hotplace-img-item-vue v-for="fileInfo in hotplace.fileInfos" :key="fileInfo.originalFile"></hotplace-img-item-vue>
+      </div>
+      <div>
+        <button
+            type="button"
+            id="btn-move-list"
+            class="btn btn-outline-primary mb-3 mr-3"
+            @click="moveListHotplace"
+          >
+            목록
+        </button>
+        <button
+            type="button"
+            id="btn-move-list"
+            class="btn btn-outline-primary mb-3 mr-3"
+            
+          >
+            글수정
+        </button>
+        <button
+            type="button"
+            id="btn-move-list"
+            class="btn btn-outline-primary mb-3 mr-3"
+            
+          >
+            글삭제
+        </button>
+      </div>
+    </span>
+    <!-- 핫플 정보, 위치 -->
+    <span>
+      <div>
+      </div>
+      <div>
+      </div>
+    </span>
   </div>
 </template>
 
 <script>
 import { getHotplace } from '@/api/hotplace';
+import HotplaceImgItemVue from './item/HotplaceImgItem.vue';
 
 export default {
   name: 'HotplaceView',
-  components: {},
+  components: {
+    HotplaceImgItemVue,
+  },
   data() {
     return {
       hotplace: {},
+      hotplaceImg: "",
     };
   },
   created() {
@@ -27,7 +70,12 @@ export default {
       }
     );
   },
-  methods: {},
+  methods: {
+    moveListHotplace() {
+      alert("핫플레이스 목록 이동")
+      this.$router.push({ name: "hotplaceList" })
+    },
+  },
 };
 </script>
 
