be2e1b21b907ed6c3feaa594a2d4c6ce0322eb84
손효민 <sondoh100@naver.com>
2023-05-25 08:58:16 +0900

    Update FE - Home

--- FE/enjoytour_korea/src/components/hotplace/HotplaceList.vue
+++ FE/enjoytour_korea/src/components/hotplace/HotplaceList.vue
@@ -1,27 +1,35 @@
 <template>
-  <div class="row p-1">
-    <div>
-    <button type="button" id="btn-mv-register" class="btn btn-outline-primary btn-sm mr-1"
-          @click="moveWrite">
-                핫플 등록하기
-          </button>
-    </div>
-    <div>
+  <b-row class = "mt-3">
+    <div class="row align-self-center mb-2" >
+        <div class="text-start">
+          <button
+            type="button"
+        id="btn-mv-register"
+        variant="outline-primary"
+        @click="moveWrite"
+      >
+        핫플 등록하기
+      </button>
+        </div>
+    <b-row>
       <div class="album">
-      <div class="container p-0 py-2">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            <hotplace-list-item v-for="hotplace in hotplaces" :key="hotplace.hotplaceNo" v-bind="hotplace"></hotplace-list-item> 
+        <div class="p-0 py-2">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <hotplace-list-item
+              v-for="hotplace in hotplaces"
+              :key="hotplace.hotplaceNo"
+              v-bind="hotplace"
+            ></hotplace-list-item>
+          </div>
         </div>
       </div>
-    </div>
-    </div>      
-    
-  </div>
+    </b-row>
+  </b-row>
 </template>
 
 <script>
 import { listHotplace } from "@/api/hotplace";
-import HotplaceListItem from './item/HotplaceListItem.vue';
+import HotplaceListItem from "./item/HotplaceListItem.vue";
 import { mapState } from "vuex";
 
 const memberStore = "memberStore";
@@ -52,8 +60,8 @@ export default {
   },
   methods: {
     moveWrite() {
-        // alert("핫플 write 이동");
-        this.$router.push({ name: "hotplaceWrite" });
+      // alert("핫플 write 이동");
+      this.$router.push({ name: "hotplaceWrite" });
     },
   },
 };
